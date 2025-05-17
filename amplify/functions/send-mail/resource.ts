250bbe39b40a96819cff02a874ba2e29306d8fa9
hello112334 <hello112334@gmail.com>
2025-05-17 16:53:42 +0900

    refactor: remove unused authentication, data, storage, and email sending resources

--- amplify/functions/send-mail/resource.ts
+++
@@ -1,8 +0,0 @@
-import { defineFunction } from "@aws-amplify/backend";
-
-export const sendEmail = defineFunction({
-  // optionally specify a name for the Function (defaults to directory name)
-  name: "send-email",
-  // optionally specify a path to your handler (defaults to "./handler.ts")
-  entry: "./handler.ts",
-});
